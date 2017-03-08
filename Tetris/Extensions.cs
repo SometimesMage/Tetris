ce15a8d10999ac7799692c02e554cf6b8c3c32b4
Nick Peterson <tacticalsk8er@gmail.com>
2017-03-07 16:25:17 -0800

    Added GameInfoView and Rectangle.addMargin() extension

---
+++ Tetris/Extensions.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Tetris {
+    public static class Extensions {
+        public static Rectangle addMargin(this Rectangle rect, int margin) {
+            rect.X += margin;
+            rect.Y += margin;
+            rect.Height -= margin * 2;
+            rect.Width -= margin * 2;
+            return rect;
+        }
+
+        public static Rectangle resizeHeightToAspectRatio(this Rectangle rect, float ratio) {
+            rect.Height = Convert.ToInt32(rect.Width / ratio);
+            return rect;
+        }
+
+        public static Rectangle reszieWidthToAspectRatio(this Rectangle rect, float ratio) {
+            rect.Width = Convert.ToInt32(rect.Height * ratio);
+            return rect;
+        }
+    }
+}
